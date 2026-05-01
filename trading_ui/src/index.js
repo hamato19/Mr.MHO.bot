2c066ae9038c45986a7e08fe20e3449592d590e9
Service <hamato19@gmail.com>
2026-05-01 12:53:23 +0300

    Update index.js

--- trading_ui/src/index.js
+++ trading_ui/src/index.js
@@ -2,10 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// هذا الملف هو حلقة الوصل بين كود React وصفحة الـ HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
