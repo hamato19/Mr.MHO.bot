d7b7c6f252eda6874685e1b5cfb93c18707821c2
Service <hamato19@gmail.com>
2026-05-09 20:12:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -99,12 +99,12 @@ async def handle_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
             database.update_user_secret_token(uid, new_token)
             webhook_text = services.format_webhook_links(uid)
             
-            # تعديل اسم الدالة هنا إلى get_main_menu
-            await query.edit_message_text(
-                text=f"🔐 <b>تم تحديث رمز الأمان!</b>\nالروابط الجديدة:\n\n<code>{webhook_text}</code>",
-                parse_mode='HTML',
-                reply_markup=keyboards.get_main_menu(uid) 
-            )
+        elif data == 'tok': # توليد رمز جديد
+            new_token = secrets.token_hex(8).upper()
+            database.update_user_secret_token(uid, new_token)
+            webhook_text = services.format_webhook_links(uid)
+            msg = await context.bot.send_message(chat_id=uid, text=f"🔐 <b>تم تحديث رمز الأمان!</b>\nالروابط الجديدة:\n\n<code>{webhook_text}</code>", parse_mode='HTML')
+            context.user_data['temp_msg_ids'].append(msg.message_id)
             return
 
         elif data == 'chs': # قنواتي
