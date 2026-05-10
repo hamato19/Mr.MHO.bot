d52c6751c5d8e274944a589f576d8d9ed1b4d1f6
Service <hamato19@gmail.com>
2026-05-10 07:14:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ async def handle_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
         
         elif data == 'adm_gen_menu': # قائمة توليد الأكواد
             await query.edit_message_text("🔑 <b>توليد أكواد اشتراك:</b>\nاختر مدة الكود المراد إنشاؤه:", parse_mode='HTML', reply_markup=keyboards.get_generation_menu())
-            
+        return
         elif data.startswith('gen_'): # تنفيذ عملية التوليد
             days = int(data.split('_')[1])
             code = f"SMO-{secrets.token_hex(4).upper()}"
